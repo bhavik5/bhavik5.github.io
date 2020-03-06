1a9f949e8b18ec10bb8dd328bd42952c1584c574
bhavik5 <s.bhavik.v.5@gmail.com>
2020-03-07 00:55:08 +0530

    Updates

---
+++ precache-manifest.1f8846cd449da1e923dcf354a06787d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e93583c55fe77867550b7f7f701911b",
+    "url": "/bhavik-portfolio/index.html"
+  },
+  {
+    "revision": "dbdbae1ea1d88d77b7a0",
+    "url": "/bhavik-portfolio/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/bhavik-portfolio/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bhavik-portfolio/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbdbae1ea1d88d77b7a0",
+    "url": "/bhavik-portfolio/static/js/main.e997e761.chunk.js"
+  },
+  {
+    "revision": "71c40ac383d42860f2d4",
+    "url": "/bhavik-portfolio/static/js/runtime-main.35f8b56e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fb77b1d72775ddaaaa875387fb000fc7.js"
+  "/bhavik-portfolio/precache-manifest.1f8846cd449da1e923dcf354a06787d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhavik-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
