6c16c298c99975bd9755ce63881cf6c0d24807e7
bhavik5 <s.bhavik.v.5@gmail.com>
2020-03-07 00:51:00 +0530

    Updates

---
+++ precache-manifest.372b056e2f5bde5ff870fe177dc6a5af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83f3ab79f1d570e08345f5ef0220fee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fc770bc3a60db002cca",
+    "url": "/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fc770bc3a60db002cca",
+    "url": "/static/js/main.4b4e024e.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
